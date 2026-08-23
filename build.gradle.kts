a0d203dd44791ccab198478d40da62c9df77907b
rohit02xo-lab <rohit02.xo@gmail.com>
2026-08-23 14:37:57 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,4 @@
 plugins {
-    id("com.android.application")
-    kotlin("android")
-}
-
-android {
-    namespace = "com.scrollcheck.app"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.scrollcheck.app"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    id("com.android.application") version "8.7.3" apply false
+    kotlin("android") version "2.0.21" apply false
 }
