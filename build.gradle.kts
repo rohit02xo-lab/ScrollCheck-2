7d6c7fb0363c4ce00bf1e6e40a84bc5986859fe8
rohit02xo-lab <rohit02.xo@gmail.com>
2026-08-23 14:11:09 +0530

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,26 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    kotlin("android") version "2.0.21" apply false
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    namespace = "com.scrollcheck.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.scrollcheck.app"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
